66043c0e6c7cb3a238c196d5e9cf6b304f49b754
AnkurIbySarkar <60110929+AnkurIbySarkar@users.noreply.github.com>
2022-02-16 14:34:39 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,18 +5,12 @@ import setuptools
 setuptools.setup(
     name="codeswitch",
     version="1.1",
-    author="Sagor Sarker",
-    author_email="sagorhem3532@gmail.com",
-    description="Code Switch is a NLP tool can use for language identification, pos tagging, name entity recognition, sentiment analysis of code mixed data.",
     long_description=codecs.open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/sagorbrur/codeswitch",
-    license="MIT",
+    url="https://github.com/AnkurIbySarkar/codeswitch",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
     ],
     python_requires='>=3.5',
     install_requires=[
