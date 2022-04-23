e42cf226a268b29c5ccbb47e0095bb0dc98b9526
AnkurIbySarkar <60110929+AnkurIbySarkar@users.noreply.github.com>
2022-04-23 12:17:57 +0530

    Update codeswitch.py

--- codeswitch/codeswitch.py
+++ codeswitch/codeswitch.py
@@ -3,8 +3,8 @@ from transformers import pipeline, AutoModelForTokenClassification, AutoTokenize
 class LanguageIdentification(object):
         elif self.language == "hin-eng":
             print("Downloading pretrained model. It will take time according to model size and your internet speed")
-            self.tokenizer = AutoTokenizer.from_pretrained("sagorsarker/codeswitch-hineng-lid-lince")
-            self.model = AutoModelForTokenClassification.from_pretrained("sagorsarker/codeswitch-hineng-lid-lince")
+            self.tokenizer = AutoTokenizer.from_pretrained("AnkurIbySarkar/codeswitch-hineng-lid-lince")
+            self.model = AutoModelForTokenClassification.from_pretrained("AnkurIbySarkar/codeswitch-hineng-lid-lince")
             print("Model Download Completed!")
     def identify(self, text):
         lid_model = pipeline('ner', model=self.model, tokenizer=self.tokenizer)
